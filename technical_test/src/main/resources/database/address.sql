12e37e16cf33628335a98cc3f2193cd04d4f4a5b
kopeleaa <kopiloviceleonora@gmail.com>
2024-10-25 13:30:22 +0200

    Upload postman requests and sql databases

---
+++ technical_test/src/main/resources/database/address.sql
@@ -0,0 +1,2 @@
+INSERT INTO technical_test.dbo.address (id, address_type, city, house_number, street, zip_code, person_id) VALUES (1, null, N'London', 334, N'Baker', N'12345', null);
+INSERT INTO technical_test.dbo.address (id, address_type, city, house_number, street, zip_code, person_id) VALUES (2, null, N'London', 112, N'Citadel', N'12345', null);
